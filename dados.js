e95dbd886217f0e56b4a2634c6035c8aa3084a5a
Amanda Barros <133551817+AmandaBarros0@users.noreply.github.com>
2024-09-08 21:58:31 -0400

    Projeto: Aprendendo a Programar com Alura e Google Gemini
    Este projeto foi desenvolvido durante a Imersão Alura com o objetivo de aplicar os conhecimentos de HTML, CSS e JavaScript em um tema livre. A proposta escolhida é uma aplicação sobre o Google Gemini, uma poderosa ferramenta de IA. A aplicação é dividida em três vertentes principais que tornam o aprendizado prático e dinâmico:
    
    História do Gemini: Um mergulho na origem e desenvolvimento do Google Gemini, explorando como essa tecnologia revolucionou o uso da inteligência artificial.
    
    Como usar a IA: Um guia interativo que ensina o usuário a utilizar o Gemini, apresentando dicas e truques para potencializar o uso da inteligência artificial em diferentes contextos.
    
    Primeiros Passos com a IA: Nesta parte, o usuário é conduzido diretamente ao Gemini, podendo começar a interagir e criar seus próprios comandos e soluções através da ferramenta, tornando a experiência prática e imersiva desde o início.
    
    Com uma interface amigável e intuitiva, este projeto não apenas ensina a base de programação, mas também demonstra na prática como o uso de inteligência artificial pode ser integrado em diversas áreas.
    
    Frontend: HTML, CSS, JavaScript.
    
    Este projeto foi projetado para proporcionar uma experiência de aprendizado fluida e estimulante, com uma interface moderna e interativa que guia o usuário desde o entendimento teórico até a prática aplicada com IA.

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "História Gemini",
+        descricao: "O Gemini, a mais nova criação do Google, representa um avanço significativo na inteligência artificial. Desenvolvido para simular a inteligência humana, o Gemini desafia os limites do que as máquinas podem fazer. Descubra como essa inovação está redefinindo o futuro da tecnologia.",
+        link: "https://blog.google/intl/pt-br/produtos/apresentando-a-busca-na-era-gemini/",
+        tags: "gemini google criação da google "
+    },
+    {
+        titulo: "Usando a nova IA do Google",
+        descricao: "A inovação do Google, projetada para transformar a IA, redefine como interagimos com a tecnologia. Essa nova inteligência é capaz de entender e responder de forma cada vez mais humana, desafiando os limites do que pode ser feito no campo da inteligência artificial.",
+        link: "https://canaltech.com.br/inteligencia-artificial/como-usar-a-ia-gemini-do-google/",
+        tags: "assistente inteligente ferramenta para tudo resolver problemas facilitar a vida"
+    },
+    {
+        titulo: "Explore a nova era da IA",
+        descricao: "Quer experimentar o potencial dessa ferramenta? Visite o site oficial e interaja com uma das mais poderosas tecnologias já criadas, capaz de transformar suas ideias em soluções práticas. Veja como essa ferramenta está ajudando a criar novas possibilidades no mundo atual.",
+        link: "https://gemini.google.com/?hl=pt-BR",
+        tags: "experimentar a ia inovação tecnológica futuro da tecnologia ferramenta do futuro"
+    }
+];
